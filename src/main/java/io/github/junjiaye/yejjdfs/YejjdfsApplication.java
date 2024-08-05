84ccaca05a152e4cb79badea51d67287e821668e
叶佳骏 <yejjr@yonyou.com>
2024-08-05 17:10:35 +0800

    init

---
+++ src/main/java/io/github/junjiaye/yejjdfs/YejjdfsApplication.java
@@ -0,0 +1,13 @@
+package io.github.junjiaye.yejjdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YejjdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YejjdfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/junjiaye/yejjdfs/YejjdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junjiaye.yejjdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YejjdfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
